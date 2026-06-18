57b3c2ce9c54247bc3d01783b44dcedc5cc2008c
yuna <micha0215@tukorea.ac.kr>
2026-06-18 15:37:37 +0900

    style: PlayCard 컴포넌트 함수명, round 수정

--- frontend/src/common/card/PlayCard.jsx
+++ frontend/src/common/card/PlayCard.jsx
@@ -1,9 +1,9 @@
 // 홈 - 놀거리 섹션 카드 (핫플/전시/자연)
-export default function TopCard({ icon, text, onClick }) {
+export default function PlayCard({ icon, text, onClick }) {
   return (
     <button
       onClick={onClick}
-      className=" flex flex-col items-center justify-center w-[88px] h-[92px] rounded-xl bg-white shadow-[0px_2px_4px_rgba(0,0,0,0.25)]"
+      className=" flex flex-col items-center justify-center w-[88px] h-[92px] rounded-5 bg-white shadow-sm"
     >
       <p className="text-24-sb">{icon}</p>
       <p className="text-black1 text-12-sb">{text}</p>
